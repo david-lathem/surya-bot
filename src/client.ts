69b83a6050c048036ff3dac90b991960a03e96c6
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-07-15 23:57:17 +0500

    s

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,9 @@
 import { Client, GatewayIntentBits } from "discord.js";
-
+import dotenv from "dotenv";
 import registerEventsOnClient from "./utils/registrars/registerEvents.js";
+import path from "node:path";
+
+dotenv.config({ path: path.join(import.meta.dirname, "..", ".env") });
 
 const client = new Client({
   intents: [
